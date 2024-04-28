d20bc63df52a2cf14715f5f8fdf464e007794831
Linda Hrdinová <hrlin@seznam.cz>
2024-04-29 00:11:31 +0200

    homework random Citat

--- src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -5,16 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
+import java.util.Random;
 
 @Controller
 public class MainController {
 
+    private final Random random = new Random();
+
     @GetMapping("/")
     public ModelAndView citaty() {
+        int nahodneCislo = random.nextInt(1, 9);
+        String[] nahodnyCitat = {
+                "Debugging /de·bugh·ing/ (verb): The Classic Mystery Game where you are the detective, the victim, and the murderer.",
+                "A user interface is like a joke. If you have to explain it, it's not that good.",
+                "To replace programmers with robots, clients will have to accurately describe what they want. We're safe.",
+                "I have a joke on programming but it only works on my computer.",
+                "99 little bugs in the code, 99 bugs in the code. Take one down, patch it around. 127 little bugs in the code…",
+                "When I wrote this code, only God & I understood what it did. Now… Only God knows.",
+                "Programmer (noun.): A machine that turns coffee into code.",
+                "Real programmers count from 0."};
         ModelAndView result = new ModelAndView("citatyTemplate");
-        LocalDate aktualniDatum = LocalDate.now();
-        result.addObject("citaty", aktualniDatum);
-
+        result.addObject("nahodnyCitat", nahodnyCitat[nahodneCislo]);
         return result;
     }
 }
