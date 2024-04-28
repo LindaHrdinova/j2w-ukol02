41d48dc1d61111b27e6993a455716c3dd73c2fd3
Linda Hrdinová <hrlin@seznam.cz>
2024-04-29 00:42:23 +0200

    homework random Img better

--- src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -14,7 +14,18 @@ public class MainController {
 
     @GetMapping("/")
     public ModelAndView citaty() {
-        int nahodneCislo = random.nextInt(0, 8);
+
+        String[] nahodnyCitat = {
+                "Debugging /de·bugh·ing/ (verb): The Classic Mystery Game where you are the detective, the victim, and the murderer.",
+                "A user interface is like a joke. If you have to explain it, it's not that good.",
+                "To replace programmers with robots, clients will have to accurately describe what they want. We're safe.",
+                "I have a joke on programming but it only works on my computer.",
+                "99 little bugs in the code, 99 bugs in the code. Take one down, patch it around. 127 little bugs in the code…",
+                "When I wrote this code, only God & I understood what it did. Now… Only God knows.",
+                "Programmer (noun.): A machine that turns coffee into code.",
+                "Real programmers count from 0."};
+
+        int nahodneCislo = random.nextInt(0, nahodnyCitat.length);
         String nahodnyObrazek = "";
         switch (nahodneCislo) {
             case 1:
@@ -43,15 +54,6 @@ public class MainController {
                 break;
         }
 
-        String[] nahodnyCitat = {
-                "Debugging /de·bugh·ing/ (verb): The Classic Mystery Game where you are the detective, the victim, and the murderer.",
-                "A user interface is like a joke. If you have to explain it, it's not that good.",
-                "To replace programmers with robots, clients will have to accurately describe what they want. We're safe.",
-                "I have a joke on programming but it only works on my computer.",
-                "99 little bugs in the code, 99 bugs in the code. Take one down, patch it around. 127 little bugs in the code…",
-                "When I wrote this code, only God & I understood what it did. Now… Only God knows.",
-                "Programmer (noun.): A machine that turns coffee into code.",
-                "Real programmers count from 0."};
         ModelAndView result = new ModelAndView("citatyTemplate");
         result.addObject("nahodnyCitat", nahodnyCitat[nahodneCislo]);
         result.addObject("nahodnyObrazek", nahodnyObrazek);
